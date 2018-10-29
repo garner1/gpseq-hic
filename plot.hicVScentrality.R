76b444627552088f46efc1f3c36af127439cdab4
garner1 <silvano.garnerone@gmail.com>
2018-10-29 10:55:49 +0100

    minor

--- plot.hicVScentrality.R
+++ plot.hicVScentrality.R
@@ -1,28 +1,27 @@
-# library
 library(ggplot2)
 library(Matrix)
 library(dplyr)
 
 # create a dataset
-data_intra = read.table("/home/garner1/Work/dataset/gpseq+hic/layer-delta-medianCount.intra.symmetric.tsv",header=FALSE,col.names = c('centrality','layer_distance','HiC'))
+data_intra = read.table("/home/garner1/Work/dataset/gpseq+hic/layer-delta-medianCount.intra.symm.tsv",header=FALSE,col.names = c('centrality','layer_distance','HiC'))
 data_intra$layer_distance <- as.factor(data_intra$layer_distance)
 intra_perc <- group_by(data_intra, centrality) %>% mutate(percent = HiC/sum(HiC))
 
-data_inter = read.table("/home/garner1/Work/dataset/gpseq+hic/layer-delta-medianCount.inter.symmetric.tsv",header=FALSE,col.names = c('centrality','layer_distance','HiC'))
+data_inter = read.table("/home/garner1/Work/dataset/gpseq+hic/layer-delta-medianCount.inter.symm.tsv",header=FALSE,col.names = c('centrality','layer_distance','HiC'))
 data_inter$layer_distance <- as.factor(data_inter$layer_distance)
 inter_perc <- group_by(data_inter, centrality) %>% mutate(percent = HiC/sum(HiC))
 
-ggplot(data_intra, aes(fill=layer_distance, y=HiC, x=centrality)) + geom_area(aes(fill = layer_distance, group = layer_distance),position = "stack")+scale_fill_brewer(palette = "Set3")
+ggplot(data_intra, aes(fill=layer_distance, y=HiC, x=centrality)) + geom_col(aes(fill = layer_distance, group = layer_distance),position = "stack")+scale_fill_brewer(palette = "Set3")
 ggsave(filename = "intra-chrom.hicVScentrality.pdf", device = cairo_pdf)
 dev.off()
-ggplot(intra_perc, aes(fill=layer_distance, y=percent, x=centrality)) + geom_area(aes(fill = layer_distance, group = layer_distance),position = "stack")+scale_fill_brewer(palette = "Set3")
+ggplot(intra_perc, aes(fill=layer_distance, y=percent, x=centrality)) + geom_col(aes(fill = layer_distance, group = layer_distance),position = "stack")+scale_fill_brewer(palette = "Set3")
 ggsave(filename = "intra-chrom.hicVScentrality.percentage.pdf", device = cairo_pdf)
 dev.off()
 
-ggplot(data_inter, aes(fill=layer_distance, y=HiC, x=centrality)) + geom_area(aes(fill = layer_distance, group = layer_distance),position = "stack")+scale_fill_brewer(palette = "Set3")
+ggplot(data_inter, aes(fill=layer_distance, y=HiC, x=centrality)) + geom_col(aes(fill = layer_distance, group = layer_distance),position = "stack")+scale_fill_brewer(palette = "Set3")
 ggsave(filename = "inter-chrom.hicVScentrality.pdf", device = cairo_pdf)
 dev.off()
-ggplot(inter_perc, aes(fill=layer_distance, y=percent, x=centrality)) + geom_area(aes(fill = layer_distance, group = layer_distance),position = "stack")+scale_fill_brewer(palette = "Set3")
+ggplot(inter_perc, aes(fill=layer_distance, y=percent, x=centrality)) + geom_col(aes(fill = layer_distance, group = layer_distance),position = "stack")+scale_fill_brewer(palette = "Set3")
 ggsave(filename = "inter-chrom.hicVScentrality.percentage.pdf", device = cairo_pdf)
 dev.off()
 
